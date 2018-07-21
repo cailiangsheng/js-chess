7804184398a393260b2d462f6b45ba6812af4add
Larry Cai <larry.cai@sigma.name>
2018-07-21 19:16:32 +0800

    initial tattoo for special chess grid

--- src/components/chess-grid/index.js
+++ src/components/chess-grid/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import ChessTattoo from 'components/chess-tattoo'
 import './style.less'
 
 const numColumns = 8 + 1
@@ -12,13 +13,17 @@ const isZuPosition = (rowIndex, cellIndex) => {
   return [3, 6].includes(rowIndex) && [0, 2, 4, 6, 8].includes(cellIndex)
 }
 
+const needsTattoo = (rowIndex, cellIndex) => {
+  return isPaoPosition(rowIndex, cellIndex)
+    || isZuPosition(rowIndex, cellIndex)
+}
+
 const getCells = (rowIndex) => {
   return Array
     .from({length: numColumns})
-    .map((v, i) => <td key={i} index={i} className={classNames('cell', {
-        'pao-pos': isPaoPosition(rowIndex, i),
-        'zu-pos': isZuPosition(rowIndex, i)
-    })} />)
+    .map((v, i) => <td key={i} index={i} className='cell'>
+      { needsTattoo(rowIndex, i) && <ChessTattoo /> }
+    </td>)
 }
 
 const getRows = () => {
---
+++ src/components/chess-tattoo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.less'
+
+const ChessTattoo = () => {
+  return <div className='chess-tattoo'>
+    <div className='top-left' />
+    <div className='top-right' />
+    <div className='bottom-left' />
+    <div className='bottom-right' />
+  </div>
+}
+
+export default ChessTattoo
