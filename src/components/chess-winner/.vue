5e443ec60296ed99cf315dbd0649acf8deb30451
cailiangsheng <larry.cai@sigma.name>
2018-07-29 17:04:14 +0800

    initial vue

--- src/components/chess-winner/.vue
+++ src/components/chess-winner/.vue
@@ -1,5 +1,16 @@
 <template>
-	<div class="chess-winner" />
+	<div class="chess-winner" v-bind:class="winnerColor" />
 </template>
 
 <style lang="less" src="./style.less"></style>
+
+<script>
+	export default {
+		props: {
+			winnerColor: {
+				type: String,
+				default: ''
+			}
+		}
+	}
+</script>
