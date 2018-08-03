bbe7dfc3fc68cc89c80fe9db9d5d9e79a366874d
Larry Cai <larry.cai@sigma.name>
2018-08-03 17:00:46 +0800

    change for player

--- src/components/chess-status/index.js
+++ src/components/chess-status/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import ChessMan from 'components/chess-man'
+import {getName} from 'components/chess-man/util'
+import CHESS_MAN from 'components/chess-man/consts'
 import './style.less'
 
 const ChessStatus = ({winnerColor, playerColor}) => {
+  const playerName = playerColor && getName({type: CHESS_MAN.TYPE.JIANG, color: playerColor})
   return <div className='chess-status'>
     {winnerColor && <div className={classNames('winner', winnerColor)} />}
-    {playerColor && <div className={classNames('player', playerColor)} />}
+    {playerName && <div className='player'><ChessMan name={playerName} /></div>}
   </div>
 }
 
