c286b32897fdac2010b40ca1d36f7a6ac5afeda6
Larry Cai <larry.cai@sigma.name>
2018-08-07 18:00:06 +0800

    fix style for chess man count

--- src/components/chess-man-list/index.js
+++ src/components/chess-man-list/index.js
@@ -2,14 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ChessMan from 'components/chess-man'
 import {getType} from 'components/chess-man/util'
+import _ from 'lodash'
 import './style.less'
 
-const ChessManList = ({names}) => {
+const sortNames = (names) => {
   names.sort((name1, name2) => getType(name1) - getType(name2))
+}
+
+const ChessManList = ({names}) => {
+  const countNames = _.countBy(names, name => name)
+  const uniqNames = _.uniq(names)
+  sortNames(uniqNames)
   return <div className='chess-man-list'>
     {
-      names.map((name, index) => (
-        <ChessMan name={name} key={index} />
+      uniqNames.map((name, index) => (
+        <div className='chess-man-list-item'>
+          <ChessMan name={name} key={index} />
+          <div className='chess-man-count'>
+            {countNames[name]}
+          </div>
+        </div>
       ))
     }
   </div>
