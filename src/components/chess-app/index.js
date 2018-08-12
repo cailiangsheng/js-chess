498ec3fa0d8567575cf72755ad39e87ff043521c
Larry Cai <larry.cai@sigma.name>
2018-08-12 13:14:00 +0800

    refacoring for react

---
+++ src/components/chess-app/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import ChessGame from 'components/chess-game'
+import ChessBoard from 'components/chess-board'
+import ChessBoardFlip from 'components/chess-board-flip'
+import ChessGameFlip from 'components/chess-game-flip'
+
+const ChessApp = () => {
+    return <Router>
+        <div>
+        <Route exact path='/' component={ChessGame} />
+        <Route path='/flip' component={ChessGameFlip} />
+        <Route path='/board' component={ChessBoard} />
+        <Route path='/board-flip' component={ChessBoardFlip} />
+        </div>
+    </Router>
+}
+
+export default ChessApp
--- src/index.react.js
+++ src/index.react.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {HashRouter as Router, Route} from 'react-router-dom'
-import ChessGame from 'components/chess-game'
-import ChessBoard from 'components/chess-board'
-import ChessBoardFlip from 'components/chess-board-flip'
-import ChessGameFlip from 'components/chess-game-flip'
+import ChessApp from 'components/chess-app'
 import './style.less'
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path='/' component={ChessGame} />
-      <Route path='/flip' component={ChessGameFlip} />
-      <Route path='/board' component={ChessBoard} />
-      <Route path='/board-flip' component={ChessBoardFlip} />
-    </div>
-  </Router>,
+  <ChessApp />,
   document.querySelector("#root")
 )
