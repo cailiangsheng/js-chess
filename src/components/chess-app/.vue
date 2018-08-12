b50c39401d86a4d46a3f49ccdee4d434a8adf984
Larry Cai <larry.cai@sigma.name>
2018-08-12 13:05:43 +0800

    refactoring for vue

---
+++ src/components/chess-app/.vue
@@ -0,0 +1,27 @@
+<template>
+    <router-view />
+</template>
+
+<script>
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ChessGame from 'components/chess-game/.vue'
+import ChessBoard from 'components/chess-board/.vue'
+import ChessBoardFlip from 'components/chess-board-flip/.vue'
+import ChessGameFlip from 'components/chess-game-flip/.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+        { path: '/', component: ChessGame },
+        { path: '/board', component: ChessBoard },
+        { path: '/board-flip', component: ChessBoardFlip },
+        { path: '/flip', component: ChessGameFlip }
+    ]
+})
+
+export default {
+    router
+}
+</script>
--- src/index.vue.js
+++ src/index.vue.js
@@ -1,26 +1,10 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import ChessGame from 'components/chess-game/.vue'
-import ChessBoard from 'components/chess-board/.vue'
-import ChessBoardFlip from 'components/chess-board-flip/.vue'
-import ChessGameFlip from 'components/chess-game-flip/.vue'
+import ChessApp from 'components/chess-app/.vue'
 import './style.less'
 
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  routes: [
-    { path: '/', component: ChessGame },
-    { path: '/board', component: ChessBoard },
-    { path: '/board-flip', component: ChessBoardFlip },
-    { path: '/flip', component: ChessGameFlip }
-  ]
-})
-
-new Vue({
-  router,
-  template: '<router-view />'
-})
+new Vue(
+  ChessApp
+)
 .$mount(
   document.querySelector('#root')
 )
