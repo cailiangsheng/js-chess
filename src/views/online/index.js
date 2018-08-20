e297e2967559ff8e10b4a11eea05d743c78038ad
Larry Cai <larry.cai@sigma.name>
2018-08-20 18:46:29 +0800

    init views

---
+++ src/views/online/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ChessGameOnline = (props) => {
+  return <span>Online Game</span>
+}
+
+export default ChessGameOnline
---
+++ src/views/standalone/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ChessGameStandalone = (props) => {
+  return <span>Standalone Game</span>
+}
+
+export default ChessGameStandalone
