8fed2104f1b14d3dcc08d8fd6bb2e3869b0cced2
Larry Cai <larry.cai@sigma.name>
2018-08-31 15:19:25 +0800

    fix input for room id

--- src/views/chess-game-online/entry/index.js
+++ src/views/chess-game-online/entry/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BasicButton from 'components/button/basic'
 import ToggleButton from 'components/button/toggle'
+import _ from 'lodash'
 import './style.less'
 
 const colorOptions = [
@@ -13,12 +14,37 @@ const roomOptions = [
 	{label: '指定', value: 'specify'}
 ]
 
-const ChessGameEntry = (props) => {
-	return <ul className='chess-game-entry'>
-		<li>棋子: <ToggleButton options={colorOptions} /></li>
-		<li>房间: <ToggleButton options={roomOptions} /></li>
-		<li><BasicButton label='确定' /></li>
-	</ul>
+class ChessGameEntry extends React.Component {
+	constructor (props) {
+		super(props)
+		this.input = React.createRef()
+		this.state = {
+			roomMode: 'random'
+		}
+	}
+
+	_onRoomModeChange = (value) => {
+		this.setState({
+			roomMode: value
+		})
+	}
+
+	_onConfirm = () => {
+		const roomId = _.get(this.input, 'current.value')
+		console.log(roomId)
+	}
+
+	render () {
+		const needsRoomId = this.state.roomMode === 'specify'
+		return <ul className='chess-game-entry'>
+			<li>棋子: <ToggleButton options={colorOptions} /></li>
+			<li>房间: <ToggleButton options={roomOptions} onChange={this._onRoomModeChange} /></li>
+			{
+				needsRoomId && <li><input type='text' ref={this.input} placeholder='房间号' /></li>
+			}
+			<li><BasicButton label='确定' onClick={this._onConfirm} /></li>
+		</ul>
+	}
 }
 
 export default ChessGameEntry
